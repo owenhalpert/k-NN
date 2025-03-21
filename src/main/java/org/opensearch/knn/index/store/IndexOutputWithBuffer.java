2509840b3ce9ae7e47cfbdba6c8d69f223e12474
owenhalpert <ohalpert@gmail.com>
2025-03-21 14:24:41 -0700

    Param tuning cluster
    Signed-off-by: owenhalpert <ohalpert@gmail.com>

--- src/main/java/org/opensearch/knn/index/store/IndexOutputWithBuffer.java
+++ src/main/java/org/opensearch/knn/index/store/IndexOutputWithBuffer.java
@@ -5,7 +5,9 @@
 
 package org.opensearch.knn.index.store;
 
+import lombok.extern.log4j.Log4j2;
 import org.apache.lucene.store.IndexOutput;
+import org.opensearch.knn.index.KNNSettings;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,6 +16,7 @@ import java.io.InputStream;
  * Wrapper around {@link IndexOutput} to perform writes in a buffered manner. This class is created per flush/merge, and may be used twice if
  * {@link org.opensearch.knn.index.codec.nativeindex.remote.RemoteIndexBuildStrategy} needs to fall back to a different build strategy.
  */
+@Log4j2
 public class IndexOutputWithBuffer {
     // Underlying `IndexOutput` obtained from Lucene's Directory.
     private IndexOutput indexOutput;
@@ -21,12 +24,13 @@ public class IndexOutputWithBuffer {
     // Allocating 64KB here since it show better performance in NMSLIB with the size. (We had slightly improvement in FAISS than having 4KB)
     // NMSLIB writes an adjacent list size first, then followed by serializing the list. Since we usually have more adjacent lists, having
     // 64KB to accumulate bytes as possible to reduce the times of calling `writeBytes`.
-    private static final int CHUNK_SIZE = 64 * 1024;
+    // private static final int CHUNK_SIZE = 64 * 1024;
     private final byte[] buffer;
 
     public IndexOutputWithBuffer(IndexOutput indexOutput) {
         this.indexOutput = indexOutput;
-        this.buffer = new byte[CHUNK_SIZE];
+        int bufferSize = (int) KNNSettings.getDownloadBufferSize().getBytes();
+        this.buffer = new byte[bufferSize];
     }
 
     // This method will be called in JNI layer which precisely knows
@@ -48,6 +52,7 @@ public class IndexOutputWithBuffer {
      * @see IndexOutputWithBuffer#writeFromStreamWithBuffer(InputStream, byte[])
      */
     public void writeFromStreamWithBuffer(InputStream inputStream) throws IOException {
+        log.info("Writing from stream to index output with buffer size: {}", this.buffer.length);
         writeFromStreamWithBuffer(inputStream, this.buffer);
     }
 
@@ -68,8 +73,8 @@ public class IndexOutputWithBuffer {
         // InputStream uses -1 indicates there are no more bytes to be read
         while (bytesRead != -1) {
             // Try to read CHUNK_SIZE into the buffer. The actual amount read may be less.
-            bytesRead = inputStream.read(outputBuffer, 0, CHUNK_SIZE);
-            assert bytesRead <= CHUNK_SIZE;
+            bytesRead = inputStream.read(outputBuffer, 0, this.buffer.length);
+            assert bytesRead <= this.buffer.length;
             // However many bytes we read, write it to the IndexOutput if != -1
             if (bytesRead != -1) {
                 indexOutput.writeBytes(outputBuffer, 0, bytesRead);
